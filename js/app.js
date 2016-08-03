1c951eba152bf379f55be9ac81969296fc767a68
Kyle Hersey <khersey@Kyles-MacBook-Air.local>
2016-08-03 14:32:49 -0400

    changes home.html to welcome.html in app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
 	$stateProvider
 	.state('home', {
-		templateUrl : 'home.html',
+		templateUrl : 'welcome.html',
 		url         : '/home'
 	})
 	.state('resume', {
